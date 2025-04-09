3e0b90cbae88671e9bd3fd1c69e85dda315c9e95
0adev <ayoubaguirar44@gmail.com>
2025-04-09 11:58:31 +0100

    Update the index.js file

--- src/containers/features/index.js
+++ src/containers/features/index.js
@@ -1,6 +1,6 @@
 export { default as FeatureImgOne } from "../../assets/features-img01.jpg";
 export { default as TimerSvg } from "../../assets/timer-forward-.svg";
-export { default as FeatureImgThree } from "../../assets/features-img02.jpg";
+export { default as FeatureImgTwo } from "../../assets/features-img02.jpg";
 export { default as QualityMedalSvg } from "../../assets/quality-medal-.svg";
-export { default as FeatureImgTwo } from "../../assets/asset-40.webp";
+export { default as FeatureImgThree } from "../../assets/asset-40.webp";
 export { default as PerfumeSvg } from "../../assets/perfume-svgrepo-com.svg";
