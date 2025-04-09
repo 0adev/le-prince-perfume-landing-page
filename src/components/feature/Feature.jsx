e592de3fb7d8e2b168e0baa23fa01332d6bd0bed
0adev <ayoubaguirar44@gmail.com>
2025-04-09 11:35:48 +0100

    Update UI for the features section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,15 +3,15 @@ import "./feature.css";
 
 function Feature({ image, icon, title, paragraph }) {
   return (
-    <div className="princedlf-feature">
-      <div className="princedlf__feature-container d-grid gap-8">
+    <div className="princedlf__feature">
+      <div className="princedlf__feature-container d-grid gap-7 place-items-center">
         <div className="princedlf__feature-container-image position-relative">
           <img src={image} alt="" />
           <img src={icon} alt="icon" className="position-absolute" />
         </div>
-        <div className="princedlf__feature-content d-grid gap-3 ">
+        <div className="princedlf__feature-content d-grid place-content-center gap-3 ">
           <h3 className="title marhey-ff fw-semibold fs-4 text-red">{title}</h3>
-          <p className="fs-3 fw-semibold text-balance">{paragraph}</p>
+          <p className="fs-3 fw-semibold text-wrap">{paragraph}</p>
         </div>
       </div>
     </div>
