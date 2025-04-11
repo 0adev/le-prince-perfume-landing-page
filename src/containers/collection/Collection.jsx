de942419c300de0bb622d01132c15224980a174f
0adev <ayoubaguirar44@gmail.com>
2025-04-11 02:21:26 +0100

    Update the UI and add more features

--- src/containers/collection/Collection.jsx
+++ src/containers/collection/Collection.jsx
@@ -1,17 +1,46 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
+import { RiCloseLine } from "react-icons/ri";
 import SectionHeader from "../../components/sectionHeader/SectionHeader";
 import "./collection.css";
-import { MenCollectionImage, WomenCollectionImage, ArrowSvg } from ".";
+import {
+  MenCollectionImage,
+  WomenCollectionImage,
+  ArrowSvg,
+  MenCatalog,
+  WomenCatalog,
+} from ".";
 
 const Collection = () => {
-  // const [showMenCollection, setShowMenCollection] = useState(false);
+  const [showMenCollection, setShowMenCollection] = useState(false);
+  const [showWomenCollection, setShowWomenCollection] = useState(false);
+  // const [closeCatalog, setCloseCtalog] = useState(true);
 
   return (
-    <div className="princedlf__collection section__margin o-hidden">
+    <div className="princedlf__collection section__margin">
       <SectionHeader title={"قائمة العطور"} subTitle={"استمتع بأرقى الروائح"} />
-      <div className="princedlf__collection-container d-flex justify-content-center align-items-center gap-7">
+      <div className="princedlf__collection-container d-flex justify-content-center align-items-center gap-7 position-relative">
+        {showMenCollection && (
+          <div className="men-catalog position-absolute z-999">
+            <div className="men-catalog-container">
+              <button onClick={() => setShowMenCollection(false)}>
+                <RiCloseLine size={27} />
+              </button>
+              <img src={MenCatalog} alt="" />
+            </div>
+          </div>
+        )}
+        {showWomenCollection && (
+          <div className="women-catalog position-absolute z-999">
+            <div className="women-catalog-container">
+              <button onClick={() => setShowWomenCollection(false)}>
+                <RiCloseLine size={27} />
+              </button>
+              <img src={WomenCatalog} alt="" />
+            </div>
+          </div>
+        )}
         {/* <!-- men collection --> */}
-        <div className="men position-relative" data-aos="fade-left">
+        <div className="men position-relative" data-aos="fade-up">
           <div className="container position-relative o-hidden w-100">
             <div className="card-cover">
               <img src={MenCollectionImage} alt="" />
@@ -23,7 +52,7 @@ const Collection = () => {
               <a
                 href="#MenCollection"
                 className="text-light fw-medium fs-2 text-deco position-absolute d-flex gap-2"
-                // onClick={() => setShowMenCollection(true)}
+                onClick={() => setShowMenCollection(true)}
               >
                 قائمة العطور <img src={ArrowSvg} alt="" />
               </a>
@@ -31,7 +60,7 @@ const Collection = () => {
           </div>
         </div>
         {/* <!-- women collection --> */}
-        <div className="women" data-aos="fade-right">
+        <div className="women" data-aos="fade-down">
           <div className="container position-relative o-hidden w-100">
             <div className="card-cover">
               <img src={WomenCollectionImage} alt="" />
@@ -41,8 +70,9 @@ const Collection = () => {
                 عطور نسائية
               </h3>
               <a
-                href="#"
+                href="#WomenCollection"
                 className="text-light fw-medium fs-2 text-deco position-absolute d-flex gap-2"
+                onClick={() => setShowWomenCollection(true)}
               >
                 قائمة العطور <img src={ArrowSvg} alt="" />
               </a>
--- src/containers/collection/index.js
+++ src/containers/collection/index.js
@@ -1,3 +1,5 @@
 export { default as MenCollectionImage } from "../../assets/men-collection-img.jpg";
 export { default as WomenCollectionImage } from "../../assets/women-collection-img.jpg";
 export { default as ArrowSvg } from "../../assets/arrow-down.svg";
+export { default as MenCatalog } from "../../assets/men-catalog.png";
+export { default as WomenCatalog } from "../../assets/women-catalog.png";
